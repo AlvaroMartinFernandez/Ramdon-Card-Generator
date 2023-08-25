3e88e34e54ae10db539a56006476ea915a550de7
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-08-25 14:46:36 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -1,13 +1,49 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-import table from "./components/tablero.js";
-import card from "./components/card.js";
-import { deck, SUIT, CARD, ramdomCard } from "./utils/randomcard.js";
-table();
-card();
-window.onload = function() {};
+window.onload = function() {
+  //Declaramos las funciones
+  function randomPalo() {
+    const palos = ["♦", "♥", "♠", "♣"];
+
+    let indexPalo = Math.floor(Math.random() * palos.length);
+    return palos[indexPalo];
+  }
+
+  function randomNumbers() {
+    const numbers = ["A", "2", "3", "4", "5", "6", "8", "9", "J", "Q", "K"];
+    let indexNumber = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumber];
+  }
+
+  //Guardamos las funciones
+
+  let generarPalo = randomPalo();
+  let generarNumero = randomNumbers();
+
+  //Selección de elementos del HTML
+  let top = document.querySelector(".top");
+  let bottom = document.querySelector(".bottom");
+  let numero = document.querySelector(".numbers");
+
+  //Insertar las variables en el HTML
+  top.innerHTML = generarPalo;
+  bottom.innerHTML = generarPalo;
+  numero.innerHTML = generarNumero;
+
+  //Codicionales para asignar color a los palos
+  if (top.innerHTML === "♦" || top.innerHTML === "♥") {
+    top.style.color = "red";
+  } else {
+    top.style.color = "black";
+  }
+
+  if (bottom.innerHTML === "♦" || bottom.innerHTML === "♥") {
+    bottom.style.color = "red";
+  } else {
+    bottom.style.color = "black";
+  }
+};
